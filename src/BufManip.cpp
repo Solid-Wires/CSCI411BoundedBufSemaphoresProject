4c72b38839cbff8ee34bf9a9c798e3ad1d5acf0b
Solid-Wires <solid.wires@gmail.com>
2024-02-20 02:18:01 -0500

    copy

--- src/BufManip.cpp
+++ src/BufManip.cpp
@@ -1,12 +1,13 @@
 #include "../inc/Common.h"
+using namespace std;
 
 // Defined within namespace
 namespace BoundedBufProj {
-    
+
     // Insert an item into the buffer.
     int InsertItem(buffer_item item) {
         try {
-            std::memcpy(shared.buf, item, BUFF_SIZE);
+            copy(begin(item), end(item), begin(shared.buf));
             return 0;
         } catch (...) {}
         return -1;
@@ -15,7 +16,7 @@ namespace BoundedBufProj {
     // Pop an item from the buffer.
     int RemoveItem(buffer_item *item) {
         try {
-            std::memcpy(item, shared.buf, BUFF_SIZE);
+            copy(begin(shared.buf), end(shared.buf), begin(item));
             return 0;
         } catch (...) {}
         return -1;
