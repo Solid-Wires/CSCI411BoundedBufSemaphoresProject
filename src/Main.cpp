ce79a893571679905efaf77addd826defb10534e
Solid-Wires <solid.wires@gmail.com>
2024-02-20 02:03:45 -0500

    Does this work?

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,26 +1,27 @@
 #include "../inc/Common.h"
-using namespace BoundedBufProj;
 using namespace std;
 
-sbuf_t shared;
+namespace BoundedBufProj {
+    sbuf_t shared;
 
-// Main point of entry
-int main()
-{
-    pthread_t idP, idC;
-    int index;
+    // Main point of entry
+    int main()
+    {
+        pthread_t idP, idC;
+        int index;
 
-    // Initialize buffer
+        // Initialize buffer
 
-    /* Insert code here to initialize semaphores */
+        /* Insert code here to initialize semaphores */
 
-    for (index = 0; index < NP; index++)
-    {  
-        /* Create a new producer */
-        pthread_create(&idP, NULL, Producer, (void*)index);
+        for (index = 0; index < NP; index++)
+        {  
+            /* Create a new producer */
+            pthread_create(&idP, NULL, Producer, (void*)index);
+        }
+
+        /* Insert code here to create NC consumers */
+
+        pthread_exit(NULL);
     }
-
-    /* Insert code here to create NC consumers */
-
-    pthread_exit(NULL);
 }
