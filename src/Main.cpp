6f81f7453a51038c51aac662de70210de1eec89c
Solid-Wires <solid.wires@gmail.com>
2024-02-19 23:41:35 -0500

    Make sure it can compile first

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,13 +4,14 @@ int main()
 {
     pthread_t idP, idC;
     int index;
-    
+
     /* Insert code here to initialize semaphores */
 
     for (index = 0; index < NP; index++)
     {  
-       /* Create a new producer */
-       pthread_create(&idP, NULL, Producer, (void*)index);
+        /* Create a new producer */
+        //pthread_create(&idP, NULL, Producer, (void*)index);
+        cout << "Running" << endl;
     }
 
     /* Insert code here to create NC consumers */
