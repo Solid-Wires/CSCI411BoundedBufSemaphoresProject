327a60f8f234cf6f487d24e8c4822703d69fe0a5
Solid-Wires <solid.wires@gmail.com>
2024-02-20 02:07:50 -0500

    now does it recognize this?

--- src/BufManip.cpp
+++ src/BufManip.cpp
@@ -3,7 +3,7 @@ namespace BoundedBufProj {
     // Insert an item into the buffer.
     int InsertItem(buffer_item item) {
         try {
-            std::memcpy(shared.buf, item, BUFF_SIZE);
+            shared.buf = item;
             return 0;
         } catch (...) {}
         return -1;
@@ -12,7 +12,7 @@ namespace BoundedBufProj {
     // Pop an item from the buffer.
     int RemoveItem(buffer_item *item) {
         try {
-            std::memcpy(item, shared.buf, BUFF_SIZE);
+            item = shared.buf;
             return 0;
         } catch (...) {}
         return -1;
