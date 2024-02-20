ef982118126024ed50f143b6d478c0060ed39eb3
Solid-Wires <solid.wires@gmail.com>
2024-02-20 01:31:58 -0500

    revert

--- src/BufManip.cpp
+++ src/BufManip.cpp
@@ -2,7 +2,7 @@
 using namespace BoundedBufProj;
 
 // Insert an item into the buffer.
-int BoundedBufProj::InsertItem(buffer_item item[BUFF_SIZE]) {
+int BoundedBufProj::InsertItem(buffer_item item) {
     try {
         shared.buf = item;
         return 0;
@@ -11,7 +11,7 @@ int BoundedBufProj::InsertItem(buffer_item item[BUFF_SIZE]) {
 }
 
 // Pop an item from the buffer.
-int BoundedBufProj::RemoveItem(buffer_item *item[BUFF_SIZE]) {
+int BoundedBufProj::RemoveItem(buffer_item *item) {
     try {
         //item = shared.buf;
         return 0;
