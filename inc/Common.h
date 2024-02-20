9a98511f7bb15f326bcc9b82e930092f89cc5025
Solid-Wires <solid.wires@gmail.com>
2024-02-20 00:01:57 -0500

    Trying this now

--- inc/Common.h
+++ inc/Common.h
@@ -18,7 +18,15 @@
 #define NITERS      4		/* number of items produced/consumed */
 
 // Fields and structs
-int test;
+typedef struct {
+    int buf[BUFF_SIZE];   /* shared var */
+    int in;         	  /* buf[in%BUFF_SIZE] is the first empty slot */
+    int out;        	  /* buf[out%BUFF_SIZE] is the first full slot */
+    sem_t full;     	  /* keep track of the number of full spots */
+    sem_t empty;    	  /* keep track of the number of empty spots */
+    sem_t mutex;    	  /* enforce mutual exclusion to shared data */
+} sbuf_t;
+extern sbuf_t shared;
 
 // Functions
 // Since producer and consumer are so simple, they don't really need their own headers
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,6 @@
 #include "../inc/Common.h"
 using namespace std;
 
-typedef struct {
-    int buf[BUFF_SIZE];   /* shared var */
-    int in;         	  /* buf[in%BUFF_SIZE] is the first empty slot */
-    int out;        	  /* buf[out%BUFF_SIZE] is the first full slot */
-    sem_t full;     	  /* keep track of the number of full spots */
-    sem_t empty;    	  /* keep track of the number of empty spots */
-    sem_t mutex;    	  /* enforce mutual exclusion to shared data */
-} sbuf_t;
 sbuf_t shared;
 
 int main()
