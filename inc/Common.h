243eb427554d10856cebad1ca5ddb05bb8d0e4cb
Solid-Wires <solid.wires@gmail.com>
2024-02-20 00:01:05 -0500

    testing

--- inc/Common.h
+++ inc/Common.h
@@ -18,15 +18,7 @@
 #define NITERS      4		/* number of items produced/consumed */
 
 // Fields and structs
-typedef struct {
-    int buf[BUFF_SIZE];   /* shared var */
-    int in;         	  /* buf[in%BUFF_SIZE] is the first empty slot */
-    int out;        	  /* buf[out%BUFF_SIZE] is the first full slot */
-    sem_t full;     	  /* keep track of the number of full spots */
-    sem_t empty;    	  /* keep track of the number of empty spots */
-    sem_t mutex;    	  /* enforce mutual exclusion to shared data */
-} sbuf_t;
-sbuf_t shared;
+int test;
 
 // Functions
 // Since producer and consumer are so simple, they don't really need their own headers
