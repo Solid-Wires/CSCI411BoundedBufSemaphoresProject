a5eee0fb25631b33bfa374c51f04c76c2b1831ea
Solid-Wires <solid.wires@gmail.com>
2024-02-20 22:23:09 -0500

    Made this a pointer instead

--- src/Consumer.cpp
+++ src/Consumer.cpp
@@ -7,8 +7,8 @@ namespace BoundedBufProj {
     // The consumer's start routine
     void *Consumer(void *arg)
     {
-        // Item it is consuming
-        buffer_item item;
+        // Item it is consuming (a pointer)
+        buffer_item *item;
 
         // Loops until it consumes all it's going to consume
         for (int i = 0; i < NITERS; i++) {
@@ -16,7 +16,7 @@ namespace BoundedBufProj {
             // Basically ensures that the producers and consumers are not synchronized
             //sleep(sleepTime);
 
-            r_code code = RemoveItem(&item);
+            r_code code = RemoveItem(item);
             if (code == -1) {
                 cerr << "ERR: Consumer failed to extract an item!" << endl;
             }
