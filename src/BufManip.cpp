b9e4edb886b1b0b0d028ed7133f2567aeb434e0b
Solid-Wires <solid.wires@gmail.com>
2024-02-20 21:12:06 -0500

    Slight adjustments to BufManip

--- src/BufManip.cpp
+++ src/BufManip.cpp
@@ -3,19 +3,23 @@ using namespace std;
 
 // Defined within namespace
 namespace BoundedBufProj {
-    // Insert an item into the buffer.
+    // Insert an item into the first empty slot on the buffer.
+    // shared.in gets incremented, and Modulo BUFF_SIZE wraps around
     r_code InsertItem(buffer_item item) {
         try {
             shared.buf[shared.in%BUFF_SIZE] = item;
+            shared.in++;
             return 0;
         } catch (...) {}
         return -1;
     }
 
-    // Take an item out of the buffer.
+    // Take an item out of the first full slot from the buffer.
+    // shared.out gets incremented, and Modulo BUFF_SIZE wraps around
     r_code RemoveItem(buffer_item *item) {
         try {
             item = &shared.buf[shared.out%BUFF_SIZE];
+            shared.out++;
             return 0;
         } catch (...) {}
         return -1;
