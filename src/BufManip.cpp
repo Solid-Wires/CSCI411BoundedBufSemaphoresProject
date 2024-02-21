6e9d02f88306a79bf8b3d66d6c7f7c6e0d989f63
Solid-Wires <solid.wires@gmail.com>
2024-02-20 22:12:32 -0500

    endl oops

--- src/BufManip.cpp
+++ src/BufManip.cpp
@@ -20,7 +20,7 @@ namespace BoundedBufProj {
         try {
             cout << "item we're removing: " << shared.buf[shared.out%BUFF_SIZE] << endl;
             item = &shared.buf[shared.out%BUFF_SIZE];
-            cout << "item we just removed: " << item;
+            cout << "item we just removed: " << item << endl;
             shared.out++;
             return 0;
         } catch (...) {}
