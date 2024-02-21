2fc7981671652b24f90e69995f2ccc0397cc9148
Solid-Wires <solid.wires@gmail.com>
2024-02-21 01:38:51 -0500

    Let's see if this is really getting consumed

--- src/Consumer.cpp
+++ src/Consumer.cpp
@@ -35,7 +35,7 @@ namespace BoundedBufProj {
                 cout << " >> " << "Consumer " << (int)arg << " consumed " << *item << endl;
             }
             // Consume the item. Delicious.
-            *item = 0;
+            *item = -1;
             shared.out++;
             sem_post(&shared.empty); // Increment an empty spot
 
