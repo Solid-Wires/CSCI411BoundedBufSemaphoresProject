9f7e6997a30b31379ed71ed8f83b580b340d8894
Solid-Wires <solid.wires@gmail.com>
2024-02-21 00:31:22 -0500

    deref?

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,21 +15,21 @@ int main(int argc, char *argv[])
     int numProducerThreads = NP;
     int numConsumerThreads = NC;
     try {
-        if (size(argv) > 1) {
+        if (size(*argv) > 1) {
             // Sleep time (optional)
             secRunTime = stoi(argv[1]);
             if (secRunTime < 1) {
                 throw invalid_argument("ERR: Cannot make run time less than 1");
             }
         }
-        else if (size(argv) > 2) {
+        else if (size(*argv) > 2) {
             // Number of producer threads (optional)
             numProducerThreads = stoi(argv[2]);
             if (numProducerThreads < 1) {
                 throw invalid_argument("ERR: Cannot make number of producer threads made less than 1");
             }
         }
-        else if (size(argv) > 3) {
+        else if (size(*argv) > 3) {
             // Number of consumer threads (optional)
             numConsumerThreads = stoi(argv[3]);
             if (numConsumerThreads < 1) {
