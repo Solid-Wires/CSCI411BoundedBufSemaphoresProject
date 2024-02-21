100dbc0ab70611e1ebd2ad32ab3b8a2e65b8a494
Solid-Wires <solid.wires@gmail.com>
2024-02-20 21:24:34 -0500

    Worked on consumer

--- src/Consumer.cpp
+++ src/Consumer.cpp
@@ -7,7 +7,23 @@ namespace BoundedBufProj {
     // The consumer's start routine
     void *Consumer(void *arg)
     {
-        /* Fill in the code here */
+        // Item it is consuming
+        buffer_item item;
+
+        // Loops until it consumes all it's going to consume
+        for (i = 0; i < NITERS; i++) {
+            /* Interleave producer and consumer execution */
+            // Basically ensures that the producers and consumers are not synchronized
+            //sleep(sleepTime);
+
+            r_code code = RemoveItem(&item);
+            if (code == -1) {
+                cerr << "ERR: Consumer failed to extract an item!" << endl;
+            }
+            else {
+                cout << "Consumer consumed " << item << endl;
+            }
+        }
     }
 
 }
